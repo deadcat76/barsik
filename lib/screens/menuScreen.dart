2ad0c9d7972ba20ee98765e928d759cdb4eec68c
uxur19 <Kristiha2208@mail.ru>
2024-05-13 16:55:27 +0700

    menuscreen

--- lib/screens/menuScreen.dart
+++ lib/screens/menuScreen.dart
@@ -1,6 +1,8 @@
 import 'package:barsik/project/routes/app_route_constants.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -19,54 +21,73 @@ class _MenuScreenState extends State<MenuScreen> {
       body: SingleChildScrollView(
         child: Stack(
           children: [
+            const Image(
+              image: AssetImage('assets/back_menu.jpg'),
+              fit: BoxFit.cover,
+            ),
             Container(
-              color: HexColor("#558384"),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 55, right: 25),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Stack(
-                      children: [
-                        Positioned(
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  InkWell(
-                                      onTap: () {
-                                        GoRouter.of(context).pushNamed(
-                                            MyAppRoutesConstants
-                                                .gameChangeRoute);
-                                      },
-                                      child: SvgPicture.asset(
-                                          'assets/left_button_game.svg',
-                                          width: 69.12,
-                                          height: 120)),
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              Row(
-                                children: [
-                                  InkWell(
+              padding: const EdgeInsets.only(top: 40, left: 110),
+              child: Text('ДЕТСКОЕ',
+                style: TextStyle(
+                  fontFamily: 'InterExtraBold',
+                  fontSize: 94,
+                  color: HexColor('#F4E6C6'),
+                ), ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 145, left: 310),
+              child: Text('МЕНЮ',
+                style: TextStyle(
+                  fontFamily: 'InterExtraBold',
+                  fontSize: 94,
+                  color: HexColor('#F4E6C6'),
+                ), ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 55, right: 25),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Positioned(
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                InkWell(
+                                    onTap: () {
+                                      GoRouter.of(context).pushNamed(
+                                          MyAppRoutesConstants.gameChangeRoute);
+                                    },
+                                    child: SvgPicture.asset(
+                                        'assets/left_button_game.svg',
+                                        width: 69.12,
+                                        height: 120)),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                InkWell(
                                     onTap: () {
                                       GoRouter.of(context).pushNamed(
                                           MyAppRoutesConstants.menuRoute);
                                     },
-                                      child: SvgPicture.asset(
-                                          'assets/left_button_food_active.svg',
-                                          width: 69.12,
-                                          height: 120)),
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              Row(
-                                children: [
-                                  InkWell(
+                                    child: SvgPicture.asset(
+                                        'assets/left_button_food_active.svg',
+                                        width: 69.12,
+                                        height: 120)),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                InkWell(
                                     onTap: () {
                                       GoRouter.of(context).pushNamed(
                                           MyAppRoutesConstants
@@ -76,63 +97,69 @@ class _MenuScreenState extends State<MenuScreen> {
                                         'assets/left_button_movie.svg',
                                         width: 69.12,
                                         height: 120)),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 110),
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Text('ДЕТСКОЕ МЕНЮ',
-                                      style: TextStyle(
-                                        fontFamily: 'InterExtraBold',
-                                        fontSize: 80,
-                                        fontWeight: FontWeight.w900,
-                                        color: HexColor('#F4E6C6'),
-                                      )),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                InkWell(
-                                    onTap: () {
-                                      GoRouter.of(context).pushNamed(
-                                          MyAppRoutesConstants.ageEditRoute);
-                                    },
-                                    child: SvgPicture.asset(
-                                        'assets/settings.svg',
-                                        width: 90,
-                                        height: 90)),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                InkWell(
-                                    onTap: () {
-                                      GoRouter.of(context).pushNamed(
-                                          MyAppRoutesConstants.ageSetRoute);
-                                    },
-                                    child: SvgPicture.asset('assets/sound.svg',
-                                        width: 90, height: 90)),
                               ],
                             ),
                           ],
                         ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                      GridView.count(
+                        padding: const EdgeInsets.only(
+                            left: 100, right: 100, top: 180),
+                        crossAxisCount: 2,
+                        physics: const ScrollPhysics(),
+                        shrinkWrap: true,
+                        childAspectRatio: 1.2 / 1,
+                        mainAxisSpacing: 80,
+                        children: List.generate(
+                            18,
+                            (index) => SizedBox(
+                                  child: Card(
+                                    elevation: 0,
+                                    color: Colors.transparent,
+                                    child: Center(
+                                      child: Text(
+                                        'Это $index карточка',
+                                        style: const TextStyle(
+                                          fontSize: 20,
+                                          fontFamily: 'Montserrat',
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )),
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              InkWell(
+                                  onTap: () {
+                                    GoRouter.of(context).pushNamed(
+                                        MyAppRoutesConstants.ageEditRoute);
+                                  },
+                                  child: SvgPicture.asset('assets/settings.svg',
+                                      width: 90, height: 90)),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              InkWell(
+                                  onTap: () {
+                                    GoRouter.of(context).pushNamed(
+                                        MyAppRoutesConstants.ageSetRoute);
+                                  },
+                                  child: SvgPicture.asset('assets/sound.svg',
+                                      width: 90, height: 90)),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
